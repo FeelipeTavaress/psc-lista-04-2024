dab93aee02777190247fd838cbbae77c88d24886
FeelipeTavaress <160754829+FeelipeTavaress@users.noreply.github.com>
2024-03-23 19:07:43 -0300

    Add files via upload

---
+++ Exercicio6Lista4Daniel.java
@@ -0,0 +1,20 @@
+
+package exercicio6lista4daniel;
+
+
+public class Exercicio6Lista4Daniel {
+
+    public static void main(String[] args) {
+        
+for (int numero = 1; numero <= 20; numero++)
+        {
+            System.out.println(numero);
+        }
+
+        for (int numero = 1; numero <= 20; numero++)
+        {
+            System.out.print(numero + " ");
+        }
+
+    }
+}
