788dfa3b5a184ce7c70a05a48fb97e18e4e03858
FeelipeTavaress <160754829+FeelipeTavaress@users.noreply.github.com>
2024-03-23 21:07:47 -0300

    Add files via upload

---
+++ Exercicio9Lista4Daniel.java
@@ -0,0 +1,17 @@
+
+package exercicio9lista4daniel;
+
+
+public class Exercicio9Lista4Daniel {
+
+
+    public static void main(String[] args) {
+         System.out.println("Valores ímpares:");
+for (int i = 0; i < 50; i++) {
+    if (i % 2 != 0) {
+        System.out.println(i);
+    }
+    
+}
+    }
+}
